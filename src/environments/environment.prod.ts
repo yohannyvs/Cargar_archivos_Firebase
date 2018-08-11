cab27b7ed6d8ab5706cce077dfdceb01c5ddee60
Tenryu <cursos1625@gmail.com>
2018-08-11 11:09:19 -0600

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC2NfwAPm-Dj21krBA1ofBE9gB-LHhZPc0',
+    authDomain: 'archivosfirebase.firebaseapp.com',
+    databaseURL: 'https://archivosfirebase.firebaseio.com',
+    projectId: 'archivosfirebase',
+    storageBucket: 'archivosfirebase.appspot.com',
+    messagingSenderId: '182278624110'
+  }
 };
